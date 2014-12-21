d21d94fcb544759b06ec7efa876bf51531a0139e
Jeshua Maxey <me@jeshua.co>
2014-12-21 13:32:43 +0000

    Removed unused social meta tages from head

--- header.php
+++ header.php
@@ -77,29 +77,30 @@
 	<script src="<?php echo get_template_directory_uri(); ?>/_/js/modernizr-2.6.2.dev.js"></script>
 
 	<!-- Application-specific meta tags -->
-	<?php if (true == of_get_option('meta_app_win_name')) {
+
+	<?php /* if (true == of_get_option('meta_app_win_name')) {
 	echo '<!-- Windows 8 -->';
 	echo '<meta name="application-name" content="'.of_get_option("meta_app_win_name").'" /> ';
 	echo '<meta name="msapplication-TileColor" content="'.of_get_option("meta_app_win_color").'" /> ';
 	echo '<meta name="msapplication-TileImage" content="'.of_get_option("meta_app_win_image").'" />';
-	} ?>
+	} */ ?>
 
-	<?php if (true == of_get_option('meta_app_twt_card')) {
+	<?php /* if (true == of_get_option('meta_app_twt_card')) {
 	echo '<!-- Twitter -->';
 	echo '<meta name="twitter:card" content="'.of_get_option("meta_app_twt_card").'" />';
 	echo '<meta name="twitter:site" content="'.of_get_option("meta_app_twt_site").'" />';
 	echo '<meta name="twitter:title" content="'.of_get_option("meta_app_twt_title").'">';
 	echo '<meta name="twitter:description" content="'.of_get_option("meta_app_twt_description").'" />';
 	echo '<meta name="twitter:url" content="'.of_get_option("meta_app_twt_url").'" />';
-	} ?>
+	} */ ?>
 
-	<?php if (true == of_get_option('meta_app_fb_title')) {
+	<?php /* if (true == of_get_option('meta_app_fb_title')) {
 	echo '<!-- Facebook -->';
 	echo '<meta property="og:title" content="'.of_get_option("meta_app_fb_title").'" />';
 	echo '<meta property="og:description" content="'.of_get_option("meta_app_fb_description").'" />';
 	echo '<meta property="og:url" content="'.of_get_option("meta_app_fb_url").'" />';
 	echo '<meta property="og:image" content="'.of_get_option("meta_app_fb_image").'" />';
-	} ?>
+	} */ ?>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
